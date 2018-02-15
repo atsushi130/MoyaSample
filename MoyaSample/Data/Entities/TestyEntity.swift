8681bd4aa5dd1cc8fba98dfdd4467d364db2373e
atsushi130 <atsushi_main@outlook.jp>
2018-02-15 09:07:58 +0900

    add: entity

---
+++ MoyaSample/Data/Entities/TestyEntity.swift
@@ -0,0 +1,9 @@
+//
+//  TestyEntity.swift
+//  MoyaSample
+//
+//  Created by Atsushi Miyake on 2018/02/15.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+struct TestyEntity: Decodable {}
