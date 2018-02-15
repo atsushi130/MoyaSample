5165708715a0eb60c0c409eaed60d52602a1391a
atsushi130 <atsushi_main@outlook.jp>
2018-02-15 09:08:52 +0900

    add: model

---
+++ MoyaSample/Domain/Models/Testy.swift
@@ -0,0 +1,11 @@
+//
+//  Testy.swift
+//  MoyaSample
+//
+//  Created by Atsushi Miyake on 2018/02/15.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+struct Testy {
+    init(entity: TestyEntity) {}
+}
