8ea76cb0a7bcde259751988c4fedce738f3d79de
atsushi130 <atsushi_main@outlook.jp>
2018-02-15 09:09:31 +0900

    add: sample code

---
+++ MoyaSample/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MoyaSample
+//
+//  Created by Atsushi Miyake on 2018/02/14.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        TestyRepository.shared.testy.subscribe(onNext: { testy in
+            print(testy)
+        }).disposed(by: self.disposeBag)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
