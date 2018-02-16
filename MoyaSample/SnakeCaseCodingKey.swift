9da676b9e7de935ffb0238ecb106f74939b04361
atsushi130 <atsushi_main@outlook.jp>
2018-02-16 09:22:57 +0900

    feature: SnakeCaseCodingKey

--- MoyaSample/SnakeCaseCodingKey.swift
+++ MoyaSample/SnakeCaseCodingKey.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+protocol SnakeCaseCodingKey: CodingKey {}
+extension SnakeCaseCodingKey where Self: RawRepresentable, Self.RawValue == String {
+    var stringValue: String {
+        return self.rawValue.replacingOccurrences(of: "([A-Z])", with: "_$1", options: .regularExpression).lowercased()
+    }
+}
