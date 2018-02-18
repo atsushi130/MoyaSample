75bb0d15b59b3c150115877a3cf639bd5e180ff5
atsushi130 <atsushi_main@outlook.jp>
2018-02-18 21:58:17 +0900

    rename and change directory

--- MoyaSample/Data/Service/Service.swift
+++ MoyaSample/Data/Service/Service.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2018年 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import Moya
+
+protocol Service {
+    associatedtype Endpoint: EndpointProtocol
+    var provider: MoyaProvider<Endpoint> { get }
+}
